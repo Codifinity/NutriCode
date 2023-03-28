d8da552117feb60765b6283c9df2e945ace3be51
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2023-03-28 21:58:18 +0200

    Add scanner functionality v1

--- src/screens/ScanScreen.tsx
+++ src/screens/ScanScreen.tsx
@@ -1,11 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+//modules
+import { OFF } from 'openfoodfacts-nodejs';
+import { Camera } from 'expo-camera';
+import { useIsFocused } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ScanScreen = () => {
+  const isFocused = useIsFocused();
+  const [hasPermission, setHasPermission] = React.useState<boolean | null>(
+    null
+  );
+  const [scanned, setScanned] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({
+    type,
+    data
+  }: {
+    type: string;
+    data: string;
+  }) => {
+    setScanned(true);
+    alert(`BarCode with type ${type} and data ${data} has been scanned`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>Scan Screen</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Camera
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        // style={StyleSheet.absoluteFillObject}
+        ratio="16:9"
+        zoom={0.2}
+        style={{
+          backgroundColor: 'red',
+          width: 200,
+          height: 200,
+
+          right: 0,
+          bottom: 0
+        }}
+      />
+
+      {scanned && (
+        <Button title={'Tap to scan again'} onPress={() => setScanned(false)} />
+      )}
+    </SafeAreaView>
   );
 };
 
