9af6c1f0ce3e1c5530b462cc3b14e036902d0ba6
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2023-03-29 19:50:26 +0200

    Add product screen

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import Main from './screens/Main';
+import Product from './screens/Product';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,11 @@ const Navigator = () => {
         options={{ headerShown: false }}
         name="Main"
         component={Main}
+      />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Product"
+        component={Product}
       />
     </Stack.Navigator>
   );
