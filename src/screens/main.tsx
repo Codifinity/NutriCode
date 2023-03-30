b17186afd62b0043c56d6670b4422a498b9103a9
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2023-03-30 20:47:44 +0200

    add style to tab bar

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 //screens
 import HomeScreen from './HomeScreen';
@@ -11,24 +12,28 @@ const Tab = createBottomTabNavigator();
 
 const Main = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{tabBarStyle: {backgroundColor: '#238878', paddingTop: 20, height: 100}}}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerShown: false }}
+        options={{ headerShown: false, tabBarActiveTintColor: '#4CA9EE',
+        tabBarInactiveTintColor: 'white', tabBarIcon: ({focused, color, size}) => { return <Ionicons name={"home-outline"} size={size} color={color} />} }}
       />
 
       <Tab.Screen
         name="Products"
         component={ProductsScreen}
-        options={{ headerShown: false }}
+        options={{ headerShown: false, tabBarActiveTintColor: '#4CA9EE',
+        tabBarInactiveTintColor: 'white', tabBarIcon: ({focused, color, size}) => { return <Ionicons name={"albums-outline"} size={size} color={color} />} }}
       />
       <Tab.Screen
         name="Profile"
         component={ProfileScreen}
-        options={{ headerShown: false }}
+        options={{ headerShown: false, tabBarActiveTintColor: '#4CA9EE',
+        tabBarInactiveTintColor: 'white', tabBarIcon: ({focused, color, size}) => { return <Ionicons name={"person-outline"} size={size} color={color} />}}}
       />
     </Tab.Navigator>
+
   );
 };
 
